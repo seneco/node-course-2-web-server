37a9588d12a1128fa76aa574a9aae70a46d9cd90
sob_mac <apple@hcsedv.de>
2017-06-15 14:50:15 +0200

    Mit Heroku Anbindung

--- app.js
+++ app.js
@@ -1,2 +1,47 @@
-console.log("The weather app")
+console.log("The weather app");
 
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+var app= express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + "\n");
+  next();
+});
+
+app.use(express.static(__dirname +"/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my Website"
+  });
+});
+
+app.get("/about", (req,res) => {
+   res.render("about.hbs", {
+      pageTitle: "About Page"
+   });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);  
+});
